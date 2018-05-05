abcac187b1b7707205f6c71970d3dc03d80e482d
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-05-05 16:33:59 -0300

    don't need a separate install file for ubuntu since it's superseded by boot/install.sh

--- boot/install.sh
+++ boot/install.sh
@@ -23,7 +23,7 @@ fi
 GDRIVE=/media/$(whoami)/gdrive
 sudo mkdir $GDRIVE
 echo "UUID=b72752d1-28de-39d6-8156-5fa5eff84df2 $GDRIVE hfsplus ro 0 0" | sudo tee -a /etc/fstab > /dev/null
-ln -s "$GDRIVE/music/iTunes/iTunes Music/Music/" ~/library 
+ln -s "$GDRIVE/music/iTunes/iTunes Music/Music/" ~/library
 
 # basic apt tools
 if [ $OS=ubuntu ]; then
@@ -35,9 +35,10 @@ deb http://download.videolan.org/pub/$OS/stable/ /
 deb-src http://download.videolan.org/pub/$OS/stable/ /
 " | sudo tee -a /etc/apt/sources.list > /dev/null
 wget -O - http://download.videolan.org/pub/$OS/videolan-apt.asc | sudo apt-key add -
-
 sudo apt update
-sudo apt install -y curl git htop gparted python3 nodejs curl telnet hexchat vim libdvdcss2 python-pip tree
+
+# install stuff
+sudo apt install -y curl git htop gparted python3 nodejs curl telnet hexchat vim libdvdcss2 python-pip tree cmatrix lame firefox
 pip install --upgrade pip
 
 # alias some stuff
@@ -47,9 +48,9 @@ alias node=nodejs
 alias python=python3
 alias vplay=$BASE/boot/vplay.sh
 alias inet=\"ip address | grep inet\"
-alias vu=\"amixer sset Master 5%+ 1>/dev/null\"
-alias vd=\"amixer sset Master 5%- 1>/dev/null\"
-alias vm=\"amixer sset Master 0% 1>/dev/null\"
+alias vu=\"amixer sset Master 5%+ > /dev/null\"
+alias vd=\"amixer sset Master 5%- > /dev/null\"
+alias vm=\"amixer sset Master 0% > /dev/null\"
 alias la=\"ls -a\"
 " >> ~/.bashrc
 . ~/.bashrc
@@ -64,8 +65,30 @@ git config --global credential.helper cache
 git config --global user.email "keggsmurph21@gmail.com"
 git config --global user.name "Kevin Murphy"
 
+# desktop setup
 if [ $XDG_CURRENT_DESKTOP=XFCE ]; then
+
+	# make our panel look nice :)
 	cp xfce4-panel.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 	pkill -KILL -u $(whoami)
-fi
 
+elif [ pgrep -f gnome ]; then
+
+	# set up some gnome keyboard bindings
+	KB=org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings
+
+	gsettings set "$KB/custom0/" name 'custom-mute-vol'
+	gsettings set "$KB/custom0/" binding 'F10'
+	gsettings set "$KB/custom0/" command 'amixer -D pulse sset Master 0%'
+
+	gsettings set "$KB/custom1/" name 'custom-decr-vol'
+	gsettings set "$KB/custom1/" binding 'F11'
+	gsettings set "$KB/custom1/" command 'amixer set Master playback 3dB-'
+
+	gsettings set "$KB/custom2/" name 'custom-incr-vol'
+	gsettings set "$KB/custom2/" binding 'F12'
+	gsettings set "$KB/custom2/" command 'amixer set Master playback 3dB+'
+
+	gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/','/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/','/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/']"
+
+fi
