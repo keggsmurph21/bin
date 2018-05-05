34013e537f4a221dc423899f75ee3f37b87b23be
Kevin Murphy <keggsmurph21@gmail.com>
2018-05-05 15:37:27 +0000

    more changes blah blah

--- boot/install.sh
+++ boot/install.sh
@@ -10,6 +10,7 @@ elif uname -a | grep ubuntu 1>/dev/null; then
 	OS=ubuntu
 fi
 
+# make sure we have a constistent base path
 BASE=/media/$(whoami)/live-rw
 if [ ! -d $BASE ]; then
 	# if we're here, we installed from boot/strap.sh
@@ -18,29 +19,26 @@ else
 	ln -s $BASE/git/etc/boot $BASE/boot
 	ln -s $BASE ~/persistent
 fi
+
+# set up external drive
 GDRIVE=/media/$(whoami)/gdrive
-
-# make directories
 sudo mkdir $GDRIVE
-
-# make some links
-ln -s "$GDRIVE/music/iTunes/iTunes Music/Music/" ~/library
-
-# copy some files
+echo "UUID=b72752d1-28de-39d6-8156-5fa5eff84df2 $GDRIVE hfsplus ro 0 0" | sudo tee -a /etc/fstab > /dev/null
+ln -s "$GDRIVE/music/iTunes/iTunes Music/Music/" ~/library 
 
 # basic apt tools
 if [ $OS=ubuntu ]; then
 	sudo add-apt-repository universe
-else 
-	sudo touch /etc/apt/sources.list
-	sudo chmod 0666 /etc/apt/sources.list
-	echo "deb http://download.videolan.org/pub/debian/stable/ /
-	deb http://download.videolan.org/pub/$OS/stable/ /" >> /etc/apt/sources.list
-	sudo chmod 0644 /etc/apt/sources.list
-	wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc | sudo apt-key add -
 fi
+
+echo "
+deb http://download.videolan.org/pub/$OS/stable/ /
+deb-src http://download.videolan.org/pub/$OS/stable/ /
+" | sudo tee -a /etc/apt/sources.list > /dev/null
+wget -O - http://download.videolan.org/pub/$OS/videolan-apt.asc | sudo apt-key add -
+
 sudo apt update
-sudo apt install -y curl git htop gparted python3 nodejs curl telnet hexchat vim libdvdcss2 python-pip
+sudo apt install -y curl git htop gparted python3 nodejs curl telnet hexchat vim libdvdcss2 python-pip tree
 pip install --upgrade pip
 
 # alias some stuff
@@ -52,14 +50,15 @@ alias vplay=$BASE/boot/vplay.sh
 alias inet=\"ip address | grep inet\"
 alias vu=\"amixer sset Master 5%+ 1>/dev/null\"
 alias vd=\"amixer sset Master 5%- 1>/dev/null\"
-alias vm=\"amixer sset Master 0% 1>/dev/null\"i
-alias la="ls -a"
+alias vm=\"amixer sset Master 0% 1>/dev/null\"
+alias la=\"ls -a\"
 " >> ~/.bashrc
 . ~/.bashrc
 
 # more apt
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt install -y npm
+sudo apt autoremove
 
 # git stuff
 git config --global credential.helper cache
